f6a63129f2ae3fca7105f8597024b8ae34facf52
vargen2 <hjaarn2@gmail.com>
2019-05-27 14:07:59 +0200

    Prepare remove unused code

--- AddonManager/Helpers/FlyoutMenuExtension.cs
+++ AddonManager/Helpers/FlyoutMenuExtension.cs
@@ -10,36 +10,36 @@ using Windows.UI.Xaml.Controls;
 
 namespace AddonManager.Helpers
 {
-    public static class FlyoutMenuExtension
-    {
-        public static List<MenuFlyoutItemBase> GetMyItems(DependencyObject obj)
-        {
-            return (List<MenuFlyoutItemBase>)obj.GetValue(MyItemsProperty);
-        }
-        public static void SetMyItems(DependencyObject obj, List<MenuFlyoutItemBase> value)
-        {
-            obj.SetValue(MyItemsProperty, value);
-        }
+    //public static class FlyoutMenuExtension
+    //{
+    //    public static List<MenuFlyoutItemBase> GetMyItems(DependencyObject obj)
+    //    {
+    //        return (List<MenuFlyoutItemBase>)obj.GetValue(MyItemsProperty);
+    //    }
+    //    public static void SetMyItems(DependencyObject obj, List<MenuFlyoutItemBase> value)
+    //    {
+    //        obj.SetValue(MyItemsProperty, value);
+    //    }
 
-        public static readonly DependencyProperty MyItemsProperty =
-            DependencyProperty.Register("MyItems",
-                typeof(List<MenuFlyoutItemBase>),
-                typeof(FlyoutMenuExtension),
-                new PropertyMetadata(new List<MenuFlyoutItemBase>(), (sender, e) =>
-                {
-                    var menu = sender as MenuFlyoutSubItem;
-                    menu.Items.Clear();
-                    if (e != null && e.NewValue != null)
-                    {
-                        Debug.WriteLine("hit " + e.ToString() + " " + e.NewValue.ToString());
-                        var downloads = e.NewValue as List<Download>;
-                        foreach (var item in downloads)
-                        {
-                            menu.Items.Add(new MenuFlyoutItem() { Text = item.ToString() });
-                            Debug.WriteLine(item.Version);
-                        }
-                    }
+    //    public static readonly DependencyProperty MyItemsProperty =
+    //        DependencyProperty.Register("MyItems",
+    //            typeof(List<MenuFlyoutItemBase>),
+    //            typeof(FlyoutMenuExtension),
+    //            new PropertyMetadata(new List<MenuFlyoutItemBase>(), (sender, e) =>
+    //            {
+    //                var menu = sender as MenuFlyoutSubItem;
+    //                menu.Items.Clear();
+    //                if (e != null && e.NewValue != null)
+    //                {
+    //                    Debug.WriteLine("hit " + e.ToString() + " " + e.NewValue.ToString());
+    //                    var downloads = e.NewValue as List<Download>;
+    //                    foreach (var item in downloads)
+    //                    {
+    //                        menu.Items.Add(new MenuFlyoutItem() { Text = item.ToString() });
+    //                        Debug.WriteLine(item.Version);
+    //                    }
+    //                }
 
-                }));
-    }
+    //            }));
+    //}
 }
